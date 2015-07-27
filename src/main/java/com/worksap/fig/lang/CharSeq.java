79730c524eeebf6091129db88307dcc2b4fc8742
Liu Yang <liu_yang@worksap.co.jp>
2015-07-27 11:32:31 +0800

    Add hash CharSeq.

---
+++ src/main/java/com/worksap/fig/lang/CharSeq.java
@@ -0,0 +1,56 @@
+package com.worksap.fig.lang;
+
+/**
+ * Created by liuyang on 7/27/15.
+ */
+public class CharSeq{
+    private final String str;
+
+    CharSeq(String str) {
+        if(str == null) {
+            throw new IllegalArgumentException("str should not be null");
+        }
+        this.str = str;
+    }
+
+    public CharSeq subSeq(int fromIndex, int toIndex) {
+        return new CharSeq(str.substring(fromIndex, toIndex));
+    }
+    public CharSeq subSeq(int fromIndex) {
+        return new CharSeq(str.substring(fromIndex));
+    }
+
+    public CharSeq suffix(CharSeq another){
+        return new CharSeq(str + another);
+    }
+    public CharSeq prefix(CharSeq another){
+        return new CharSeq(another + str);
+    }
+
+    public int length(){
+        return str.length();
+    }
+
+    public boolean isEmpty(){
+        return str.isEmpty();
+    }
+    public CharSeq capitalize() {
+        return isEmpty() ? this : this.subSeq(0, 1).toUpperCase().suffix(this.subSeq(1));
+    }
+
+    public CharSeq toUpperCase() {
+        return new CharSeq(str.toUpperCase());
+    }
+
+    public CharSeq toLowerCase() {
+        return new CharSeq(str.toLowerCase());
+    }
+
+    public Seq<CharSeq> split(String regex) {
+        return Seq.of(str.split(regex)).map(CharSeq::new);
+    }
+
+    public static CharSeq of(String str) {
+        return new CharSeq(str);
+    }
+}
---
+++ src/main/java/com/worksap/fig/lang/Hash.java
@@ -0,0 +1,7 @@
+package com.worksap.fig.lang;
+
+/**
+ * Created by liuyang on 7/27/15.
+ */
+public interface Hash {
+}
