6565f6eb3e93dc07aa1f460a891e0658f0108e1d
Liu Yang <liu_yang@worksap.co.jp>
2015-07-27 12:14:04 +0800

    Implement some function of Hash.

--- src/main/java/com/worksap/fig/lang/HashImpl.java
+++ src/main/java/com/worksap/fig/lang/HashImpl.java
@@ -1,9 +1,27 @@
 package com.worksap.fig.lang;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by liuyang on 7/27/15.
  */
-class HashImpl<K, V> extends HashMap<K, V> implements Hash{
+class HashImpl<K, V> extends HashMap<K, V> implements Hash<K, V> {
+
+    public HashImpl() {
+    }
+
+    public HashImpl(Map<? extends K, ? extends V> m) {
+        super(m);
+    }
+
+    @Override
+    public Seq<V> values() {
+        return Seq.of(super.values());
+    }
+
+    @Override
+    public Seq<K> keys() {
+        return Seq.of(keySet());
+    }
 }
