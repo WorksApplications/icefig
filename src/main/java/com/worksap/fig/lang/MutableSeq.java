6fd30b45be2b24419835ff20a66ad9b563cb92f9
Liu Yang <liu_yang@worksap.co.jp>
2015-08-06 14:44:17 +0800

    Redesign seq

---
+++ src/main/java/com/worksap/fig/lang/MutableSeq.java
@@ -0,0 +1,96 @@
+package com.worksap.fig.lang;
+
+import java.util.*;
+import java.util.function.*;
+
+/**
+ * An interface extending {@link com.worksap.fig.lang.Seq} (which is immutable), with additional in-place methods to change the seq itself.
+ * Those methods are generally named xxxInPlace
+ */
+public interface MutableSeq<T> extends Seq<T> {
+
+    @Override
+    <R> MutableSeq<R> map(Function<T, R> func);
+
+    @Override
+    <R> MutableSeq<R> map(BiFunction<T, Integer, R> func);
+
+    @Override
+    <R> MutableSeq<R> flatMap(Function<T, Seq<R>> func);
+
+    @Override
+    <R> MutableSeq<R> flatMap(BiFunction<T, Integer, Seq<R>> func);
+
+    @Override
+    MutableSeq<T> sample(int n);
+
+    @Override
+    MutableSeq<T> shuffle();
+
+    @Override
+    MutableSeq<MutableSeq<T>> eachCons(int n);
+
+    @Override
+    MutableSeq<T> sort(Comparator<? super T> comparator);
+
+    @Override
+    MutableSeq<T> distinct();
+
+    @Override
+    MutableSeq<T> append(T value);
+
+    @Override
+    @SuppressWarnings({"varargs", "unchecked"})
+    MutableSeq<T> append(T... values);
+
+    @Override
+    MutableSeq<T> append(Collection<? extends T> collection);
+
+    @Override
+    MutableSeq<T> append(Seq<? extends T> seq);
+
+    @Override
+    MutableSeq<T> prepend(T value);
+
+    @Override
+    @SuppressWarnings({"varargs", "unchecked"})
+    MutableSeq<T> prepend(T... values);
+
+    @Override
+    MutableSeq<T> prepend(Collection<? extends T> collection);
+
+    @Override
+    MutableSeq<T> prepend(Seq<? extends T> seq);
+
+    @Override
+    MutableSeq<T> subSeq(int fromIndex, int toIndex);
+
+    @Override
+    MutableSeq<T> reject(Predicate<T> condition);
+
+    @Override
+    MutableSeq<T> reject(BiPredicate<T, Integer> condition);
+
+    @Override
+    MutableSeq<T> filter(Predicate<T> condition);
+
+    @Override
+    MutableSeq<T> filter(BiPredicate<T, Integer> condition);
+
+    @Override
+    MutableSeq<T> repeat(int times);
+
+    @Override
+    default MutableSeq<T> compact() {
+        return this.reject(e -> e == null);
+    }
+
+    @Override
+    MutableSeq<MutableSeq<T>> eachSlice(int n);
+
+    @Override
+    MutableSeq<T> reverse();
+
+    @Override
+    MutableSeq<MutableSeq<T>> eachCombination(int n);
+}
