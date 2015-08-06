efd59a9b5617122fe3f38702c712793ecfc2f07e
Liu Yang <liu_yang@worksap.co.jp>
2015-08-06 14:44:17 +0800

    Redesign seq

--- src/main/java/com/worksap/fig/lang/SeqImpl.java
+++ src/main/java/com/worksap/fig/lang/SeqImpl.java
@@ -1,31 +1,24 @@
 package com.worksap.fig.lang;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Objects;
-import java.util.function.Consumer;
+import java.util.*;
+import java.util.function.*;
 
 /**
  * Created by liuyang on 7/23/15.
  */
-class SeqImpl<T> extends ArrayList<T> implements Seq<T> {
-    SeqImpl(Collection<? extends T> c) {
-        super(c);
+class SeqImpl<T> implements MutableSeq<T> {
+
+    private List<T> list;
+
+    public SeqImpl() {
     }
 
-    SeqImpl() {
+    public SeqImpl(List<T> list) {
+        this.list = list;
     }
 
-    SeqImpl(int size) {
-        this(size, null);
-    }
-
-    SeqImpl(int size, T defaultValue) {
-        super(size);
-        while (size > 0) {
-            this.add(defaultValue);
-            size--;
-        }
+    public SeqImpl(Collection<T> collection) {
+        this.list = new ArrayList<>(collection);
     }
 
     /**
@@ -42,15 +35,231 @@ class SeqImpl<T> extends ArrayList<T> implements Seq<T> {
         if (index >= size || index < -size)
             throw new IndexOutOfBoundsException("Index " + index + ", size " + size + ", should be within [" + (-size) + ", " + size + ")");
         if (index >= 0)
-            return super.get(index);
+            return get(index);
         else
-            return super.get(size + index);
+            return get(size + index);
     }
 
+    @Override
+    public <R> MutableSeq<R> map(Function<T, R> func) {
+        Objects.requireNonNull(func);
+        MutableSeq<R> result = new SeqImpl<>();
+        this.forEach(i -> result.append(func.apply(i)));
+        return result;
+    }
+
+    @Override
+    public <R> MutableSeq<R> map(BiFunction<T, Integer, R> func) {
+        Objects.requireNonNull(func);
+        MutableSeq<R> result = new SeqImpl<>();
+        this.forEach((s, i) -> result.append(func.apply(s, i)));
+        return result;
+    }
+
+    @Override
+    public <R> MutableSeq<R> flatMap(Function<T, Seq<R>> func) {
+        Objects.requireNonNull(func);
+        MutableSeq<R> result = new SeqImpl<>();
+        this.forEach(i -> result.append(func.apply(i)));
+        return result;
+    }
+
+    @Override
+    public <R> MutableSeq<R> flatMap(BiFunction<T, Integer, Seq<R>> func) {
+        Objects.requireNonNull(func);
+        MutableSeq<R> result = new SeqImpl<>();
+        this.forEach((s, i) -> result.append(func.apply(s, i)));
+        return result;
+    }
+
+    @Override
+    public MutableSeq<T> sample(int n) {
+        MutableSeq<T> shuffled = shuffle();
+        return shuffled.subSeq(0, Math.min(n, this.size()));
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public MutableSeq<T> shuffle() {
+        List<T> newList = new ArrayList<>(list);
+        Collections.shuffle(newList);
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<MutableSeq<T>> eachCons(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n should be positive number!");
+        }
+        MutableSeq<MutableSeq<T>> result = new SeqImpl<>();
+        for (int i = 0; i <= this.size() - n; i++) {
+            result.append(this.subSeq(i, i + n));
+        }
+        return result;
+    }
+
+    @Override
+    public MutableSeq<T> sort(Comparator<? super T> comparator) {
+        List<T> newList = new ArrayList<>(list);
+        Collections.sort(newList, comparator);
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> distinct() {
+        return new SeqImpl<>(new LinkedHashSet<>(list));
+    }
+
+    @Override
+    public MutableSeq<T> append(T value) {
+        List<T> newList = new ArrayList<>(list);
+        list.add(value);
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> append(T... values) {
+        return append(Arrays.asList(values));
+    }
+
+    @Override
+    public MutableSeq<T> append(Collection<? extends T> collection) {
+        List<T> newList = new ArrayList<>(list);
+        list.addAll(collection);
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> append(Seq<? extends T> seq) {
+        List<T> newList = new ArrayList<>(list);
+        seq.forEach((Consumer<T>) newList::add);
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> prepend(T value) {
+        List<T> newList = new ArrayList<>();
+        newList.add(value);
+        newList.addAll(list);
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> prepend(T... values) {
+        return prepend(Arrays.asList(values));
+    }
+
+    @Override
+    public MutableSeq<T> prepend(Collection<? extends T> collection) {
+        List<T> newList = new ArrayList<>();
+        newList.addAll(collection);
+        newList.addAll(list);
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> prepend(Seq<? extends T> seq) {
+        List<T> newList = new ArrayList<>();
+        seq.forEach((Consumer<T>) newList::add);
+        newList.addAll(list);
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> subSeq(int fromIndex, int toIndex) {
+        return new SeqImpl<>(list.subList(fromIndex, toIndex));
+    }
+
+    @Override
+    public MutableSeq<T> reject(Predicate<T> condition) {
+        Objects.requireNonNull(condition);
+        List<T> newList = new ArrayList<>();
+        this.forEach(e -> {
+            if (!condition.test(e))
+                newList.add(e);
+        });
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> reject(BiPredicate<T, Integer> condition) {
+        Objects.requireNonNull(condition);
+        List<T> newList = new ArrayList<>();
+        this.forEach((e, i) -> {
+            if (!condition.test(e, i))
+                newList.add(e);
+        });
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> filter(Predicate<T> condition) {
+        Objects.requireNonNull(condition);
+        List<T> newList = new ArrayList<>();
+        this.forEach(e -> {
+            if (condition.test(e))
+                newList.add(e);
+        });
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> filter(BiPredicate<T, Integer> condition) {
+        Objects.requireNonNull(condition);
+        List<T> newList = new ArrayList<>();
+        this.forEach((e, i) -> {
+            if (condition.test(e, i))
+                newList.add(e);
+        });
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<T> repeat(int times) {
+        if (times <= 0)
+            throw new IllegalArgumentException("times must be a positive number.");
+        List<T> newList = new ArrayList<>();
+        while (times > 0) {
+            newList.addAll(list);
+            times--;
+        }
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public MutableSeq<MutableSeq<T>> eachSlice(int n) {
+        if (n <= 0)
+            throw new IllegalArgumentException("n should be a positive number.");
+        List<MutableSeq<T>> newList = new ArrayList<>();
+        int size = this.size();
+        for (int i = 0; i < size; i += n) {
+            newList.add(subSeq(i, i + n > size ? size : i + n));
+        }
+        return new SeqImpl<>(newList);
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+
+    @Override
+    public MutableSeq<T> reverse() {
+        List<T> newList = new ArrayList<>();
+        for (int i = size() - 1; i >= 0; i--)
+            newList.add(this.get(i));
+        return new SeqImpl<>(newList);
+    }
+
+
     private Seq<T> indexToSeq(int[] idxs) {
-        Seq<T> result = Seq.newSeq();
+        Seq<T> result = new SeqImpl<>();
         for (int i : idxs) {
-            result.add(get(i));
+            result.append(get(i));
         }
         return result;
     }
@@ -87,9 +296,11 @@ class SeqImpl<T> extends ArrayList<T> implements Seq<T> {
         }
     }
 
-    public Seq<Seq<T>> eachCombination(int n) {
-        Seq<Seq<T>> result = Seq.newSeq();
-        forEachCombination(n, result::add);
+    public MutableSeq<MutableSeq<T>> eachCombination(int n) {
+        MutableSeq<MutableSeq<T>> result = new SeqImpl<>();
+        forEachCombination(n, s -> {
+            result.append((MutableSeq<T>) s);
+        });
         return result;
     }
 }
